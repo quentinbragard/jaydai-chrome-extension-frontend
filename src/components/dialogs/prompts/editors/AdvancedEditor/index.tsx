6542b7df71c5b3dd82c77ecbaeb5ad8f80bb7341
quentinbragard <quentinbragard@gmail.com>
2025-07-28 11:39:50 +0200

    Revert "k"

--- src/components/dialogs/prompts/editors/AdvancedEditor/index.tsx
+++ src/components/dialogs/prompts/editors/AdvancedEditor/index.tsx
@@ -128,10 +128,10 @@ export const AdvancedEditor: React.FC<AdvancedEditorProps> = ({
       list.forEach(({ key, value }) => {
         if (!value.trim()) return;
         const regex = new RegExp(`\\[${key.replace(/[.*+?^${}()|[\\]\\]/g, '\\$&')}\\]`, 'g');
-        result = result.replace(regex, () => value);
+        result = result.replace(regex, value);
 
         Object.keys(updatedCache).forEach(id => {
-          updatedCache[parseInt(id, 10)] = updatedCache[parseInt(id, 10)].replace(regex, () => value);
+          updatedCache[parseInt(id, 10)] = updatedCache[parseInt(id, 10)].replace(regex, value);
         });
       });
 
